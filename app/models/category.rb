6b096227b0671fb495ef020dbc0759789d4a247a
ales <novotny@divine.cz>
2009-05-01 08:57:31 +0000

    @ zakomentovana nepouzivana vazba habtm

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-  has_and_belongs_to_many :recipe, :join_table => "recipe_category", :foreign_key => "recipe_id"
+  # has_and_belongs_to_many :recipe, :join_table => "recipe_category", :foreign_key => "recipe_id"
 end
