5705d2ada09f91afc0da6573ea6f9cf70c36b24f
ales <novotny@divine.cz>
2009-06-01 18:55:18 +0000

    + super novy odstraneni diakritiky

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -11,7 +11,7 @@ class Ingredient < ActiveRecord::Base
   after_create :create_user_ingredient
   
   def create_user_ingredient
-    UserIngredient.create :name => Iconv.new('ascii//translit', 'utf-8').iconv(name).downcase.gsub(/[^0-9a-z-]/, ''), :ingredient_id => id
+    UserIngredient.create :name => ActiveSupport::Multibyte::Chars.new(name).normalize(:kd).to_s.downcase.gsub(/[^\x00-\x7F]/, ''), :ingredient_id => id
   end
   
   def to_s
